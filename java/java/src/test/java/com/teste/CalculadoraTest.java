a9e7e8e14b35a059aa3ced3694e0c28550775f27
Allyson de Oliveira Brandão <62918698+Allysonubius@users.noreply.github.com>
2020-06-05 19:27:40 -0300

    Finalizado video aula sobre Mocks.

--- java/java/src/test/java/com/teste/CalculadoraTest.java
+++ java/java/src/test/java/com/teste/CalculadoraTest.java
@@ -12,5 +12,23 @@ public class CalculadoraTest {
 		int soma = calc.somar("1 + 1 + 3");
 		assertEquals(5,soma);
 	}
+	@Test
+	public void testeSomarComMock() {
+		Calculadora calculadora = mock(Calculadora.class);
+		
+		//when(Calculadora.somar("1 + 2").thenReturn(10));
+		
+		int resultado = calculadorasomar("1 + 2");
+		
+		assertEquals(10,resultado);
+	}
+	private int calculadorasomar(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	private Calculadora mock(Class<Calculadora> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
